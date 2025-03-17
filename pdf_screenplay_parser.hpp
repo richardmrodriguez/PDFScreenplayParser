612bc1b4a7171946b75c9905a9e5212269d3eaef
Richard Mamaril Rodriguez <richardmamarilrodriguez@gmail.com>
2025-03-17 03:45:45 -0500

    removed unnecessary include guard

--- pdf_screenplay_parser.hpp
+++ pdf_screenplay_parser.hpp
@@ -4,9 +4,5 @@
 #include <string>
 #include <"pdf_doc.hpp">
 #include <"screenplay_doc.hpp">
-#ifndef TEXTPOS_H
-#define TEXTPOS_H
-#inlcude <"text_position.hpp">
-#endif
 
 ScreenplayDoc get_screenplay_doc() {};
