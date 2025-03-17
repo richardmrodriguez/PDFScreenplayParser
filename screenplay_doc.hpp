abfb5b91f3e1feccdb44f1060e562744d690f5da
Richard Mamaril Rodriguez <richardmamarilrodriguez@gmail.com>
2025-03-17 03:43:23 -0500

    fixed big switch case for SPTypeToString

--- screenplay_doc.hpp
+++ screenplay_doc.hpp
@@ -57,43 +57,42 @@ enum SPType {
 //TODO: MISSING TYPES
 std::string SPTypeToString(SPType type) {
     switch (type) {
-        case SP_ACTION:             return "SP_ACTION";
-        case SP_CHARACTER:          return "SP_CHARACTER";
-        case SP_CHARACTER_EXTENSION:return "SP_CHARACTER_EXTENSION";
-        case SP_PARENTHETICAL:      return "SP_PARENTHETICAL";
-        case SP_DIALOGUE:           return "SP_DIALOGUE";
-        case SP_DG_MORE_CONTINUED   return "SP_DG_MORE_CONTINUED";
+        case SP_ACTION:                 return "SP_ACTION";
+        case SP_CHARACTER:              return "SP_CHARACTER";
+        case SP_CHARACTER_EXTENSION:    return "SP_CHARACTER_EXTENSION";
+        case SP_PARENTHETICAL:          return "SP_PARENTHETICAL";
+        case SP_DIALOGUE:               return "SP_DIALOGUE";
+        case SP_DG_MORE_CONTINUED:      return "SP_DG_MORE_CONTINUED";
         //DUAL DIALOGUE
-        case SP_DD_L_CHARACTER:     return "SP_DD_L_CHARACTER";
-        case SP_DD_L_PARENTHETICAL: return "SP_DD_L_PARENTHETICAL";
-        case SP_DD_L_DIALOGUE:      return "SP_DD_L_DIALOGUE";
-        case SP_DD_L_MORE_CONTINUED return "SP_DD_L_MORE_CONTINUED";
-        case SP_DD_R_CHARACTER:     return "SP_DD_R_CHARACTER";
-        case SP_DD_R_PARENTHETICAL: return "SP_DD_R_PARENTHETICAL";
-        case SP_DD_R_DIALOGUE:      return "SP_DD_R_DIALOGUE";
-        case SP_DD_R_MORE_CONTINUED return "SP_DD_R_MORE_CONTINUED";
+        case SP_DD_L_CHARACTER:         return "SP_DD_L_CHARACTER";
+        case SP_DD_L_PARENTHETICAL:     return "SP_DD_L_PARENTHETICAL";
+        case SP_DD_L_DIALOGUE:          return "SP_DD_L_DIALOGUE";
+        case SP_DD_L_MORE_CONTINUED:    return "SP_DD_L_MORE_CONTINUED";
+        case SP_DD_R_CHARACTER:         return "SP_DD_R_CHARACTER";
+        case SP_DD_R_PARENTHETICAL:     return "SP_DD_R_PARENTHETICAL";
+        case SP_DD_R_DIALOGUE:          return "SP_DD_R_DIALOGUE";
+        case SP_DD_R_MORE_CONTINUED:    return "SP_DD_R_MORE_CONTINUED";
         // SCENE HEADING
-        case SP_SCENE_HEADING:      return "SP_SCENE_HEADING";
-        case SP_INT_EXT:            return "SP_SUBLOCATION";
-        case SP_TIME_OF_DAY:        return "SP_LOCATION";
-        case SP_LOCATION:           return "SP_TIME_OF_DAY";
-        case SP_SUBLOCATION:        return "SP_INT_EXT";
-        
-        case SP_TRANSITION:         return "SP_TRANSITION";
-        case SP_MORE_CONTINUED      return "SP_MORE_CONTINUED"
+        case SP_SCENE_HEADING:          return "SP_SCENE_HEADING";
+        case SP_INT_EXT:                return "SP_SUBLOCATION";
+        case SP_TIME_OF_DAY:            return "SP_LOCATION";
+        case SP_LOCATION:               return "SP_TIME_OF_DAY";
+        case SP_SUBLOCATION:            return "SP_INT_EXT";
+        case SP_TRANSITION:             return "SP_TRANSITION";
+        case SP_MORE_CONTINUED:         return "SP_MORE_CONTINUED"
         // TITLE PAGE
-        case TP_TITLE:              return "TP_TITLE";
-        case TP_BYLINE:             return "TP_BYLINE";
-        case TP_AUTHOR:             return "TP_AUTHOR";
-        case TP_DRAFT_DATE:         return "TP_DRAFT_DATE";
-        case TP_CONTACT:            return "TP_CONTACT";
+        case TP_TITLE:                  return "TP_TITLE";
+        case TP_BYLINE:                 return "TP_BYLINE";
+        case TP_AUTHOR:                 return "TP_AUTHOR";
+        case TP_DRAFT_DATE:             return "TP_DRAFT_DATE";
+        case TP_CONTACT:                return "TP_CONTACT";
         //-------------
-        case NON_CONTENT_TOP:       return "NON_CONTENT_TOP";
-        case NON_CONTENT_BOTTOM:    return "NON_CONTENT_BOTTOM";
-        case NON_CONTENT_LEFT:      return "NON_CONTENT_LEFT";
-        case NON_CONTENT_RIGHT:     return "NON_CONTENT_RIGHT";
-        case SP_OTHER:              return "SP_OTHER";
-        default:                    return "UNKNOWN";
+        case NON_CONTENT_TOP:           return "NON_CONTENT_TOP";
+        case NON_CONTENT_BOTTOM:        return "NON_CONTENT_BOTTOM";
+        case NON_CONTENT_LEFT:          return "NON_CONTENT_LEFT";
+        case NON_CONTENT_RIGHT:         return "NON_CONTENT_RIGHT";
+        case SP_OTHER:                  return "SP_OTHER";
+        default:                        return "UNKNOWN";
     }
 }
 
@@ -139,6 +138,6 @@ struct ScreenplayLine
 };
 
 struct ScreenplayTextElement {
-    std::string text;
+    std::string text = "";
     SPType element_type = SPType::SP_BLANK;
 }
